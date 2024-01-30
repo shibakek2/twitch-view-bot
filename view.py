e47ec388064a4f7d28118bc6250deef6f86ce92d
shibakek2 <wilhemnorman732@GMAIL.COM>
2024-01-29 23:33:34 -0500

    Create view.py
    updated view.py

---
+++ view.py
@@ -0,0 +1,12 @@
+import httpx
+import urllib.parse
+import time
+import concurrent.futures
+import random
+import string
+import traceback
+import secrets
+
+
+###REST WILL BE UPDATED ONCE THIS REPO REACHES 2500 STARS!###
+
